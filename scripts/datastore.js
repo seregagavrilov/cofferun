2650917c51c7dc5c8cbe5ed44b0d629f8b55af11
GavrilovSD <sergeigavrilov123@gmil.com>
2018-07-24 16:08:44 +0300

    add data store module and add some html on index page

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,25 +1,25 @@
 (function (window) {
   'use strict';
   var App = window.app || {};
+  var data = {};
 
 function DataStore(){
-  this.data = {};
 }
 
 DataStore.prototype.add = function (key, val){
-  this.data[key] = val;
+  data[key] = val;
 }
 
 DataStore.prototype.get = function (key){
-  return this.data[key];
+  return data[key];
 }
 
 DataStore.prototype.getAll = function(){
-  return this.data
+  return data
 }
 
 DataStore.prototype.remove = function(key){
-  delete this.data[key]
+  delete data[key]
 }
 
 App.DataStore = DataStore;
